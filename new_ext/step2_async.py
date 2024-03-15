b1859d9a85f183cb2ce5d70919d213bcf1e9dc78
guitarHero1098 <158539182+guitarHero1098@users.noreply.github.com>
2024-03-15 08:31:24 +0530

    Update step2_async.py

--- new_ext/step2_async.py
+++ new_ext/step2_async.py
@@ -1,15 +1,21 @@
 import aiohttp
 import asyncio
-import csv
+from time import perf_counter
 import json
 import pandas as pd
 import os 
+import time
 
 # Define a function to call the API and process response
-input_file = ""
-table_name = "prakash_csv_test.Organization"
-folder_path = f"C:/Users/VHALASTeratB/Desktop/Codespace/Threesteps/{table_name.split('.')[-1]}_dir"
-external_base_url = "https://developer.usastaffing.gov/api/organizations/{uniqueKey}/externalids"
+table_name = "prakash_csv_test.office"
+folder_path = f"C:/Users/VHALASTeratB/Desktop/codespace/Threesteps/outputs//{table_name.split('.')[-1]}_dir"
+external_base_url = "https://developer.usastaffing.gov/api/offices/{uniqueKey}/externalids"
+
+rejection_file = f"{folder_path}/{table_name.split('.')[-1]}_rejections.csv"
+
+def append_rejections(data):
+    with open(rejection_file, 'a') as f:
+        f.writelines(str(data)+"\n")
 
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
@@ -17,7 +23,7 @@ if not os.path.exists(folder_path):
 else:
     print(f"Folder {folder_path} already exists")
 
-json_file_path = "C:/Users/VHALASTeratB/Desktop/Codespace/Threesteps/json/seventables.json"
+json_file_path = "C:/Users/VHALASTeratB/Desktop/codespace/Threesteps/jsons/seventables.json"
 with open(json_file_path, 'r') as json_file:
     all_tab_ext_data = json.load(json_file)
 
@@ -40,29 +46,54 @@ async def pandas_process(data):
     tasks = [append_to_csv(pd.DataFrame(data[key]), key) for key in data]
     await asyncio.gather(*tasks)
 
-async def process_url(session, url):
+async def process_url(session, inp):
+    url = inp[0]
+    num = inp[1]
     try:
         async with session.get(url, headers=headers) as response:
             # Process the response, extract data
+            print(num, total_count, url)
             data = await response.json()
             await pandas_process(data)
     except Exception as e:
-        print(e)
+        print(f"Failed to read api for {url} with error : {e}")
+        append_rejections((url,e))
 
 async def main(urls):
     async with aiohttp.ClientSession() as session:
         await asyncio.gather(*[process_url(session, url) for url in urls])
 
-# List of URLs to process
-urls = []
-tenantId = 8
 
 
-for di in ext_data:
-    val = list(di.values())[0]
-    formed_unique_id = f"{tenantId}|{val}"
-    uniq_ext_url = external_base_url.replace('{uniqueKey}', formed_unique_id)
-    urls.append(uniq_ext_url)
 
-# Run the main coroutine with asyncio
-asyncio.run(main(urls))
+
+if __name__ == "__main__":
+    # List of URLs to process
+    urls = []
+    tenantId = 8
+    total_count = len(ext_data)
+    batch_size = 50
+    sleep_time = 60
+    processid = -1
+    print('-----')
+    start = perf_counter()
+    
+    # Loop over the URLs in batches
+    for i in range(0, total_count, batch_size):
+        batch_urls = []
+        for di in ext_data[i:i+batch_size]:
+            val = list(di.values())[0]
+            if val > processid:
+                formed_unique_id = f"{tenantId}|{val}"
+                uniq_ext_url = external_base_url.replace('{uniqueKey}', formed_unique_id)
+                batch_urls.append((uniq_ext_url, i))
+        
+        # Process the batch asynchronously
+        asyncio.run(main(batch_urls))
+        
+        # Sleep for 60 seconds
+        print(f"Sleeping for {sleep_time} seconds...")
+        time.sleep(sleep_time)
+        
+    end = perf_counter()
+    print(f"Time taken to read : {end - start}")
