717560b522a14c251d4336993b4a5e6be88c632b
guitarHero1098 <158539182+guitarHero1098@users.noreply.github.com>
2024-04-23 00:53:23 +0530

    Create addition_to_step3.py

---
+++ new_ext/addition_to_step3.py
@@ -0,0 +1,30 @@
+{
+    "CertificateApplication": [
+        { "filename": "certificateApplicationApplicationIDs.csv", "schema": "certificateApplicationApplicationIDs_schema" },
+        { "filename": "certificateApplicationRankingListIDs.csv", "schema": "certificateApplicationRankingListIDs_schema" }, { "filename": "certificateApplicationNewHireIDs.csv", "schema": "certificateApplicationNewHireIDs_schema" }, { "filename": "certificateApplicationRequestIDs.csv", "schema": "certificateApplicationRequestIDs_schema" }
+    ],
+    "Certificate": [
+        { "filename": "certificateVacancyIDs.csv", "schema": "certificateVacancyIDs_schema" },
+        { "filename": "certificateCertificateApplicationID.csv", "schema": "certificateCertificateApplicationID_schema" },
+        { "filename": "certificateReviewIDs.csv", "schema": "certificateReviewIDs_schema" }
+    ],
+    "Review": [
+        { "filename": "reviewAssessmentIDs.csv", "schema": "reviewAssessmentIDs_schema" },
+        { "filename": "reviewCertificateIDs.csv", "schema": "reviewCertificateIDs_schema" },
+        { "filename": "reviewRequestIDs.csv", "schema": "reviewRequestIDs_schema" },
+        { "filename": "reviewVacancyIDs.csv", "schema": "reviewVacancyIDs_schema" }
+    ],
+    "StaffingTask": [
+        { "filename": "TenantID.csv", "schema": "TenantID_schema" },
+        { "filename": "TenantID.csv", "schema": "TenantID_schema" }
+    ],
+    "Application": [
+        { "filename": "applicationOfficeIDs.csv", "schema": "applicationOfficeIDs_schema" },
+        { "filename": "applicationVacancyIDs.csv", "schema": "applicationVacancyIDs_schema" },
+        { "filename": "applicationCertificateApplicationIDs.csv", "schema": "applicationCertificateApplicationIDs_schema" }
+    ],
+    "OnboardingTasks": [
+        { "filename": "onboardingTasksNewHireID.csv", "schema": "onboardingTasksNewHireID_schema" },
+        { "filename": "onboardingTasksRequestID.csv", "schema": "onboardingTasksRequestID_schema" }
+    ]
+}
