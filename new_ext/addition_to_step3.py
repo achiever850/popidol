b65ee2ac9cb61c11b7348d7ac17320c943a0f178
guitarHero1098 <158539182+guitarHero1098@users.noreply.github.com>
2024-04-23 00:54:13 +0530

    Update addition_to_step3.py

--- new_ext/addition_to_step3.py
+++ new_ext/addition_to_step3.py
@@ -1,27 +1,27 @@
 {
     "CertificateApplication": [
-        { "filename": "certificateApplicationApplicationIDs.csv", "schema": "certificateApplicationApplicationIDs_schema" },
-        { "filename": "certificateApplicationRankingListIDs.csv", "schema": "certificateApplicationRankingListIDs_schema" }, { "filename": "certificateApplicationNewHireIDs.csv", "schema": "certificateApplicationNewHireIDs_schema" }, { "filename": "certificateApplicationRequestIDs.csv", "schema": "certificateApplicationRequestIDs_schema" }
+        { "filename": "certificateApplicationApplicationIDs.csv", "schema": "certificateApplicationApplicationID_schema" },
+        { "filename": "certificateApplicationRankingListIDs.csv", "schema": "certificateApplicationRankingListID_schema" }, { "filename": "certificateApplicationNewHireIDs.csv", "schema": "certificateApplicationNewHireID_schema" }, { "filename": "certificateApplicationRequestIDs.csv", "schema": "certificateApplicationRequestID_schema" }
     ],
     "Certificate": [
-        { "filename": "certificateVacancyIDs.csv", "schema": "certificateVacancyIDs_schema" },
+        { "filename": "certificateVacancyIDs.csv", "schema": "certificateVacancyID_schema" },
         { "filename": "certificateCertificateApplicationID.csv", "schema": "certificateCertificateApplicationID_schema" },
-        { "filename": "certificateReviewIDs.csv", "schema": "certificateReviewIDs_schema" }
+        { "filename": "certificateReviewIDs.csv", "schema": "certificateReviewID_schema" }
     ],
     "Review": [
-        { "filename": "reviewAssessmentIDs.csv", "schema": "reviewAssessmentIDs_schema" },
-        { "filename": "reviewCertificateIDs.csv", "schema": "reviewCertificateIDs_schema" },
-        { "filename": "reviewRequestIDs.csv", "schema": "reviewRequestIDs_schema" },
-        { "filename": "reviewVacancyIDs.csv", "schema": "reviewVacancyIDs_schema" }
+        { "filename": "reviewAssessmentIDs.csv", "schema": "reviewAssessmentID_schema" },
+        { "filename": "reviewCertificateIDs.csv", "schema": "reviewCertificateID_schema" },
+        { "filename": "reviewRequestIDs.csv", "schema": "reviewRequestID_schema" },
+        { "filename": "reviewVacancyIDs.csv", "schema": "reviewVacancyID_schema" }
     ],
     "StaffingTask": [
         { "filename": "TenantID.csv", "schema": "TenantID_schema" },
         { "filename": "TenantID.csv", "schema": "TenantID_schema" }
     ],
     "Application": [
-        { "filename": "applicationOfficeIDs.csv", "schema": "applicationOfficeIDs_schema" },
-        { "filename": "applicationVacancyIDs.csv", "schema": "applicationVacancyIDs_schema" },
-        { "filename": "applicationCertificateApplicationIDs.csv", "schema": "applicationCertificateApplicationIDs_schema" }
+        { "filename": "applicationOfficeIDs.csv", "schema": "applicationOfficeID_schema" },
+        { "filename": "applicationVacancyIDs.csv", "schema": "applicationVacancyID_schema" },
+        { "filename": "applicationCertificateApplicationIDs.csv", "schema": "applicationCertificateApplicationID_schema" }
     ],
     "OnboardingTasks": [
         { "filename": "onboardingTasksNewHireID.csv", "schema": "onboardingTasksNewHireID_schema" },
